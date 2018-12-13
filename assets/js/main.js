34f0efb8e0405fea208a0d137d060c97ad7fa237
Carlos Mañas <carlos@spacenomads.com>
2018-12-13 19:42:18 +0100

    Add bulk fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,52 @@
 'use strict';
 
-const api = 'https://swapi.co/api/';
-const apiSearch = '/?search=';
-const searchCats = document.querySelector('.search__cats');
+const api = 'https://swapi.co/api/people/?search=';
 const searchField = document.querySelector('.search__field');
 const searchBTN = document.querySelector('.search__btn');
 const results = document.querySelector('.results');
 
-function writeTitleResults(res) {
-  let items = '';
-  for (const r of res) {
-    items += `
-      <li class="results__item">
-        <small>Título de la película</small>
-        <h2>${r.title}</h2>
-      </li>
-    `;
-  }
-  results.innerHTML = items;
+function createPromise(url) {
+  return fetch(url).then(r=>r.json());
 }
 
-function writeNameResults(res, category) {
-  let items = '';
-  for (const r of res) {
+function writeResults(name, films) {
+  let items = `
+    <li class="results__item">
+        <h2>${name}</h2>
+        <ul>
+  `;
+  for (const r of films) {
     items += `
-      <li class="results__item">
-        <small>Nombre</small>
-        <h2>${r.name}</h2>
-    `;
-    if (category === 'people') {
-      items += `<small>Películas en las que aparece: ${r.films.length}</small>`;
-    }
-    items += `</li>`;
+      <li class="films">${r}</li>`;
   }
-  results.innerHTML = items;
+  items += `
+      </ul>
+    </li>
+  `;
+  results.innerHTML += items;
 }
 
 function searchMe() {
   const q = searchField.value;
-  const cat = searchCats.value
-
-  fetch(api+cat+apiSearch+q)
+  results.innerHTML = '';
+  fetch(api+q)
     .then(r=>r.json())
     .then(data=>{
       const apiResults = data.results;
-      console.log(apiResults);
-      if (cat === 'films') {
-        writeTitleResults(apiResults);
-      } else {
-        writeNameResults(apiResults, cat);
+      for (const a of apiResults) {
+        const name = a.name;
+        const promises = [];
+        for (const f of a.films) {
+          promises.push(createPromise(f));
+        }
+        Promise.all(promises)
+          .then(res=>{
+            let films = [];
+            for (const r of res) {
+              films.push(r.title);
+            }
+            writeResults(name, films);
+          });
       }
     });
 }
